2f61a5317d722f95127614ca2f993c143f57b4c8
Tristram Gräbener <tristram@midnighttrains.com>
2021-02-04 20:49:13 +0100

    Minimal case producing the error

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct Point {
+    lon: f64,
+    lat: f64,
+    category: String,
+}
+
 fn main() {
+    let file = std::fs::File::open("POINTXXXXX.TMI").expect("The file is missing…");
+
+    let mut rdr = csv::ReaderBuilder::new()
+        .delimiter(b';')
+        .trim(csv::Trim::All)
+        .from_reader(file);
     let from = "EPSG:28992";
     let to = "EPSG:4326";
     let amersfoort_to_wgs84 = proj::Proj::new_known_crs(&from, &to, None).unwrap();
-    let result = amersfoort_to_wgs84
-        .convert(geo_types::Point::new(228377.0, 527847.0))
-        .unwrap();
-    println!("computed: {} {}", result.x(), result.y());
+    for _point in rdr.deserialize::<Point>() {
+        let result = amersfoort_to_wgs84
+            .convert((228377.0 as f64, 527847.0 as f64))
+            .unwrap();
+        println!("computed: {} {}", result.x(), result.y());
+    }
     println!("local   : {} {}", 6.473556233092426, 52.733020858848604);
     println!("tests   : {} {}", 6.473572155087817, 52.733093297726396);
 }
